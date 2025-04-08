28bdd2e1e8785e80b5fe0f77089af4ccabd4b63e
gajawadavinay <119530616+gajawadavinay@users.noreply.github.com>
2025-04-08 20:34:38 +0000

    basic NodeJS API

--- app.js
+++ app.js
@@ -1,11 +1,108 @@
-const http = require('http');
-const url = require('url');
+// app.js
+
+const express = require('express');
+const path = require('path');
+const EventEmitter = require('events');
 
 const port = process.env.PORT || 3000;
+const app = express();
+const chatEmitter = new EventEmitter();
 
-// note that typically the variables here are `req` and `res` but we are using `request` and `response` for clarity
-// modify the server code to look like this
-const server = http.createServer(function(request, response) {
-    response.setHeader('Content-Type', 'application/json');
-    response.end(JSON.stringify({ text: 'hi', number: [1, 2, 3] }));
+// Serve static files from the "public" folder
+app.use(express.static(__dirname + '/public'));
+
+/**
+ * Responds with plain text
+ * 
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+function respondText(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hi');
+}
+
+/**
+ * Responds with JSON
+ * 
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+function respondJson(req, res) {
+  res.json({
+    text: 'hi',
+    numbers: [1, 2, 3],
   });
+}
+
+/**
+ * Responds with the input string in various formats
+ * 
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+function respondEcho(req, res) {
+  const { input = '' } = req.query;
+
+  res.json({
+    normal: input,
+    shouty: input.toUpperCase(),
+    charCount: input.length,
+    backwards: input.split('').reverse().join(''),
+  });
+}
+
+/**
+ * Serves up the chat.html file
+ * 
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+function chatApp(req, res) {
+  res.sendFile(path.join(__dirname, '/chat.html'));
+}
+
+/**
+ * Receives chat messages and emits them to all listeners
+ * 
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+function respondChat(req, res) {
+  const { message } = req.query;
+
+  chatEmitter.emit('message', message);
+  res.end();
+}
+
+/**
+ * Responds with a stream of server sent events
+ * 
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+function respondSSE(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+  });
+
+  const onMessage = message => res.write(`data: ${message}\n\n`);
+  chatEmitter.on('message', onMessage);
+
+  res.on('close', () => {
+    chatEmitter.off('message', onMessage);
+  });
+}
+
+// Register routes
+app.get('/', chatApp);
+app.get('/json', respondJson);
+app.get('/echo', respondEcho);
+app.get('/chat', respondChat);
+app.get('/sse', respondSSE);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
