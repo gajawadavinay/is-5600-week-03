b79422ff1baae353d921c5a3418e17209d822249
gajawadavinay <119530616+gajawadavinay@users.noreply.github.com>
2025-04-08 20:33:29 +0000

    json string

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const url = require('url');
 const port = process.env.PORT || 3000;
 
 // note that typically the variables here are `req` and `res` but we are using `request` and `response` for clarity
+// modify the server code to look like this
 const server = http.createServer(function(request, response) {
-  response.end("hi");
-});
-
-server.listen(port, function() {
-  console.log(`Server is listening on port ${port}`);
-});
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify({ text: 'hi', number: [1, 2, 3] }));
+  });
